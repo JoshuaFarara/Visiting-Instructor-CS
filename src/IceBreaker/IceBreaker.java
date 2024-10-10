06754ec59c3792bad5a4c77475219b5a7b5fd293
jfarara <jfarara@CSCT-BV0S2B3.ad.georgiasouthern.edu>
2024-10-10 11:05:58 -0400

    Initial Commit - IceBreaker

---
+++ src/IceBreaker/IceBreaker.java
@@ -0,0 +1,17 @@
+package IceBreaker;
+
+public class IceBreaker {
+
+	public static void main(String[] args) {
+		// roster = new String[] ; 
+		// file input 
+		System.out.println("Let's Break The Ice");
+	}
+	
+	public static String getStudentName() {
+		
+		return null;
+		
+	}
+
+}
